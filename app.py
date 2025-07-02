f8013e6ecb6c4ee89f38f1afd336ed948d659b34
Mubeen Hussain <mubeenhussain8@gmail.com>
2025-07-02 22:35:16 +0500

    add user controller

--- app.py
+++ app.py
@@ -1,96 +1,24 @@
-from fastapi import FastAPI, Path
+from fastapi import Depends, FastAPI, Path
 from typing import Optional
 from pydantic import BaseModel
+from database import get_db
+from schemas.user import UserSchema  # Import the User class
+from models.user import UserModal
+from sqlalchemy.orm import Session  # Import Session for type hinting
+from database import Base,engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-students = {
-    1:{
-        "name":"Mubeen Hussain",
-        "age":12,
-        "year":"12"
-    },
-    2:{
-        "name":"Jalal Hussain",
-        "age":12,
-        "year":"12"
-    },
-    3:{
-        "name":"Nabeel Hussain",
-        "age":12,
-        "year":"12"
-    },
-}
-
-class Student(BaseModel):
-    name:str
-    age:str
-    year:str      
-
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
-    year: Optional[str] = None
-    
-
-@app.get("/")
-def index():
-    return {
-        "name":"First data"
-    }
-    
-
-# @app.get("/get-student/{student_id}")
-# def get_student(student_id:int):
-#     print(student_id)
-#     return students[student_id]
-
-@app.get("/get-student/{student_id}")
-def get_student(student_id:int = Path(..., description="The id of student you want to view",gt=0)):
-    return students[student_id]
-
-@app.get("/get-by-name/{student_id}")
-def get_student_by_name(*,student_id: int, name:Optional[str] = None, test: int):
-    for student in students:
-        if students[student]['name'] == name:
-            return students[student]
-    return {
-        "Data":"Not found"
-    }
-    
-@app.post("/create-student/{student_id}")
-def create_student(student_id: int, student: Student):
-    if student_id in students:
-        return {
-            "Error": "Student Exists"
-        }
-    
-    students[student_id] = student
-    return students[student_id]
-
-@app.put('/update-student/{student_id}')
-def update_student(student_id: int, student:UpdateStudent):
-    if student_id not in students:
-        return {
-            "Error":"Student doesnot exist"
-        }
-    if student.name != None:
-        students[student] = student.name
-    if student.age != None:
-        students[student] = student.age
-    if student.year != None:
-        students[student] = student.year   
-        
-    students[student_id] = student
-    return students[student_id]
-
-@app.delete("/delete-student/{student_id}")
-def delete_student(student_id: int):
-    if student_id not in students:
-        return {"Error":"Student doesnot exist"}
-
-    del students[student_id]
-    return {
-        "message":
-            "student deleted successfully"
-    }
+@app.post('/user')
+def create_user(request: UserSchema, db: Session = Depends(get_db)):
+    new_user = UserModal(
+        name=request.name,
+        email=request.email,
+        password=request.password
+    )    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
